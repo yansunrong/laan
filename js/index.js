599c65418f165f6b495123345182fc93bedd00f1
yansunrong <passw0rd>
2013-06-17 22:28:09 +0800

    first import

---
+++ js/index.js
@@ -0,0 +1,104 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: yansunrong
+ * Date: 13-6-17
+ * Time: 下午8:58
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+var data = [
+    {id: 1, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
+    {id: 2, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
+    {id: 3, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
+    {id: 4, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
+    {id: 5, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
+    {id: 6, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
+    {id: 7, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
+    {id: 8, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
+    {id: 9, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
+    {id: 10, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
+    {id: 11, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
+    {id: 12, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
+    {id: 13, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
+    {id: 14, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
+    {id: 15, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
+    {id: 16, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
+    {id: 17, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
+    {id: 18, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
+    {id: 19, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
+    {id: 20, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'b'},
+    {id: 21, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'b'},
+    {id: 22, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'b'},
+    {id: 23, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'b'},
+    {id: 24, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'b'},
+    {id: 25, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'b'},
+    {id: 26, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'b'},
+    {id: 27, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'c'},
+    {id: 28, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'c'},
+    {id: 29, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'c'},
+    {id: 30, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'd'},
+    {id: 31, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'd'},
+    {id: 32, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'd'},
+    {id: 33, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'd'},
+    {id: 34, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'd'},
+    {id: 35, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'e'},
+    {id: 36, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'e'},
+    {id: 37, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'f'},
+    {id: 38, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'f'},
+    {id: 39, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'g'},
+    {id: 40, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'g'}
+]
+
+
+var itemTpl = baidu.template('item-tpl');
+
+/**
+ * 传入数据,生成列表
+ * @param list
+ */
+function renderGameList(items) {
+    $("#pagination").pagination({
+        total: items.length,
+        limit: 9,
+        offset: 0,
+        callback: function (offset) {
+            var list = items.slice(offset, offset + 9);
+            var html = '<div class="row-fluid">';
+            for (var i = 0, item; item = list[i]; i++) {
+                if (i % 3 == 0 && i > 0) {
+                    html += '</div><div class="row-fluid">';
+                }
+
+                html += itemTpl(item);
+
+            }
+            $("#list-container").html(html);
+            return false;
+        }
+    });
+}
+
+function filterbyTag(tag) {
+    var filterData = [];
+    for (var i = 0, item; item = data[i]; i++) {
+        if (tag==undefined || item.tag == tag) {
+            filterData.push(item)
+        }
+    }
+
+    renderGameList(filterData);
+}
+
+renderGameList(data);
+
+
+$("#tags").on('click',"li",function(e){
+    var li = $(this);
+    li.parent().children().removeClass("active");
+    li.addClass("active");
+    e.preventDefault();
+    e.stopPropagation();
+    filterbyTag(li.find('a').attr("data-tag"));
+});
+
