740a12b43024fa38eb13a16e2a260b8156f8326c
yansunrong <passw0rd>
2013-06-21 21:57:29 +0800

    first import

--- js/index.js
+++ js/index.js
@@ -7,52 +7,9 @@
  */
 
 
-var data = [
-    {id: 1, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
-    {id: 2, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
-    {id: 3, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
-    {id: 4, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
-    {id: 5, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
-    {id: 6, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
-    {id: 7, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
-    {id: 8, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
-    {id: 9, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
-    {id: 10, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
-    {id: 11, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
-    {id: 12, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
-    {id: 13, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
-    {id: 14, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
-    {id: 15, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
-    {id: 16, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
-    {id: 17, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
-    {id: 18, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
-    {id: 19, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'a'},
-    {id: 20, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'b'},
-    {id: 21, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'b'},
-    {id: 22, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'b'},
-    {id: 23, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'b'},
-    {id: 24, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'b'},
-    {id: 25, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'b'},
-    {id: 26, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'b'},
-    {id: 27, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'c'},
-    {id: 28, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'c'},
-    {id: 29, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'c'},
-    {id: 30, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'd'},
-    {id: 31, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'd'},
-    {id: 32, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'd'},
-    {id: 33, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'd'},
-    {id: 34, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'd'},
-    {id: 35, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'e'},
-    {id: 36, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'e'},
-    {id: 37, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'f'},
-    {id: 38, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'f'},
-    {id: 39, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'g'},
-    {id: 40, name: '游戏1', author: '作者', desc: '一个非常有意思的游戏 ,赶紧来完吧.亲!!!快啊.', tag: 'g'}
-]
-
 
 var itemTpl = baidu.template('item-tpl');
-
+var detailTpl = baidu.template("game-detail-tpl");
 /**
  * 传入数据,生成列表
  * @param list
@@ -78,11 +35,11 @@ function renderGameList(items) {
         }
     });
 }
-
-function filterbyTag(tag) {
+//通过type筛选
+function filterByType(type) {
     var filterData = [];
     for (var i = 0, item; item = data[i]; i++) {
-        if (tag==undefined || item.tag == tag) {
+        if (type==undefined || type.split(" ").indexOf(item.type) >-1 ) {
             filterData.push(item)
         }
     }
@@ -90,22 +47,48 @@ function filterbyTag(tag) {
     renderGameList(filterData);
 }
 
+
+function showDeailModal(id){
+
+    for(var i = 0;i<data.length;i++){
+        if(data[i].id == id){
+            var html = detailTpl(data[0]);
+            $("#game-detail-box").html(html);
+            $('#game-detail').modal()
+            return;
+        }
+    }
+
+}
+
+
+
+
 renderGameList(data);
 
-
-$("#tags").on('click',"li",function(e){
+//绑定标签筛选的事件
+$("#types").on('click',"li",function(e){
     var li = $(this);
     li.parent().children().removeClass("active");
     li.addClass("active");
     e.preventDefault();
     e.stopPropagation();
-    filterbyTag(li.find('a').attr("data-tag"));
+    filterByType(li.find('a').attr("data-type"));
 });
 
-//
-
-$(".setup").click(function(){
-    bootbox.alert("亲,是要安装吗!", function() {
-
-    });
+// 绑定点击安装的事件
+$(".setup").click(function(e){
+    var id = $(this).attr("data-id");
+    location.hash = id;
+    showDeailModal(id);
+    e.stopPropagation()
+    e.preventDefault()
 });
+
+
+
+
+if(location.hash){
+  //  alert(1)
+    showDeailModal(location.hash.substring(1));
+}
